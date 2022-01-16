a6b5f87f4cfb6564c79bb376b79709bdf2c787a2
David Reyes <davizi96@hotmail.com>
2022-01-16 15:38:53 -0300

    Footer and animations

---
+++ assets/js/modules/scroll-animation.js
@@ -0,0 +1,24 @@
+export default function initScrollAnimation() {
+  const scrollItems = document.querySelectorAll('[data-scroll="animation"]');
+
+  if (scrollItems.length) {
+    const windowScroll = window.innerHeight * 0.6;
+  
+    function scrollAnimation() {
+      scrollItems.forEach((item) => {
+        const itemTop = item.getBoundingClientRect().top;
+        const isItemVisible = (itemTop - windowScroll) < 0;
+        if (isItemVisible) {
+          const direction = item.dataset.animate;
+          item.classList.add('active', direction);
+        } else if (item.classList.contains('active')) {
+          item.classList.remove('active');
+        }
+      });
+    }
+  
+    scrollAnimation();
+  
+    window.addEventListener('scroll', scrollAnimation);
+  }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,10 @@ import initScrollSmooth from "./modules/scroll-smooth.js"
 import initScrollNavAnimation from "./modules/scroll-nav-animation.js"
 import initTabNav from "./modules/tabnav.js"
 import initHoverCard from "./modules/hover-card.js"
+import initScrollAnimation from "./modules/scroll-animation.js"
 
 initScrollSmooth();
 initScrollNavAnimation();
 initTabNav();
 initHoverCard();
+initScrollAnimation();
